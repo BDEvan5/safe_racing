ab4823c080bce4c7add5363b074646b963e177fd
Benjamin Evans <bdevans@sun.ac.za>
2023-11-08 10:35:39 +0200

    Block is working

--- safe_racing/BlockSim.py
+++ safe_racing/BlockSim.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import matplotlib.pyplot as plt
+from shapely.geometry import Point, Polygon
 
 SPEED = 1
 L = 0.33
@@ -15,39 +16,39 @@ def state_update(state, action):
     return np.array([x, y, theta])
 
 
-class StraightAngleSim:
-    def __init__(self, line1, line2) -> None:
+class BlockSim:
+    def __init__(self, lefts=[], rights=[]) -> None:
         self.state = np.zeros(3)
         self.history = []
-        self.l1s = line1[0]
-        self.l1e = line1[1]
-        self.l2s = line2[0]
-        self.l2e = line2[1]
+        self.lefts = lefts
+        self.rights = rights
+        self.N = len(lefts)
 
     def update(self, action):
         self.state = state_update(self.state, action)
         self.history.append(self.state)
 
         if self.check_done():
-            print(f"Massive Crash Happened!!!!! {self.state}")
+            print(f"{len(self.history)}: Massive Crash Happened!!!!! {self.state}")
             self.history.pop()
             return self.reset()
 
         return self.state
     
     def check_done(self):
-        prev_state = self.history[-2][:2]
-        if do_lines_intersect([self.l1s, self.l1e], [prev_state, self.state[:2]]):
-            return True
-        elif do_lines_intersect([self.l2s, self.l2e], [prev_state, self.state[:2]]):
-            return True
+        state_line = [self.history[-2][:2], self.state[:2]]
+        for i in range(self.N):
+            if do_lines_intersect(self.lefts[i], state_line):
+                return True
+            elif do_lines_intersect(self.rights[i], state_line):
+                return True
         return False
     
     def reset(self):
         self.state = np.zeros(3)
-        self.state[0] = 0.5
-        self.state[2] = 1.5
-        # self.state[1] = 0.5
+        self.state[0] = 2
+        # self.state[2] = 1.5
+        self.state[1] = 0.5
         self.history.append(self.state)
         return self.state
 
@@ -61,8 +62,10 @@ class StraightAngleSim:
         axis.plot(self.state[0], self.state[1], 'ro')
         axis.set_aspect('equal')
 
-        axis.plot([self.l1s[0], self.l1e[0]], [self.l1s[1], self.l1e[1]], '--', color='black')
-        axis.plot([self.l2s[0], self.l2e[0]], [self.l2s[1], self.l2e[1]], '--', color='black')
+        for i in range(self.N):
+            axis.plot([self.lefts[i][0][0], self.lefts[i][1][0]], [self.lefts[i][0][1], self.lefts[i][1][1]], '--', color='black')
+            axis.plot([self.rights[i][0][0], self.rights[i][1][0]], [self.rights[i][0][1], self.rights[i][1][1]], '--', color='black')
+
 
 def do_lines_intersect(line1, line2):
     x1, y1 = line1[0]
@@ -103,6 +106,7 @@ def do_lines_intersect(line1, line2):
         return False
     return True
 
+
 #TODO: put the do lines intersect method into the Boundary class to reduce computation
 class Boundary:
     def __init__(self, line, extension) -> None:
@@ -111,26 +115,33 @@ class Boundary:
         self.perpendicular = self.a + np.pi/2
         self.norm_vec = np.array([np.cos(self.perpendicular), np.sin(self.perpendicular)])
         self.line[1] += extension * np.array([np.cos(self.a), np.sin(self.a)])
+        
 
 
 class SafetyMask:
-    def __init__(self, max_steer, dynamics_fcn, line_l, line_r) -> None:
+    def __init__(self, max_steer, dynamics_fcn, lefts, rights) -> None:
         self.max_steer = max_steer
         self.dynamics_fcn = dynamics_fcn
         self.radius = L / np.tan(max_steer) 
+        self.bound_ind = 0
+        self.step_counter = -1
 
-        self.bound_l = Boundary(line_l, self.radius)
-        self.bound_r = Boundary(line_r, self.radius)
+        self.N = len(lefts)
+        self.left_bounds, self.right_bounds = [], []
+        self.polys = []
+        for i in range(self.N):
+            self.left_bounds.append(Boundary(lefts[i], self.radius))
+            self.right_bounds.append(Boundary(rights[i], self.radius))
+            poly = Polygon([lefts[i][0], lefts[i][1], rights[i][1], rights[i][0]])
+            self.polys.append(poly)
 
-    def enforce_safety(self, state, action, axis):        
+    def enforce_safety(self, state, action, axis):      
+        self.step_counter += 1  
         new_state = self.dynamics_fcn(state, action)
 
         angle_array = self.radius * np.array([-np.cos(np.pi/2 - new_state[2]), np.sin(np.pi/2 - new_state[2])])
         centre_l = new_state[0:2] + angle_array
         centre_r = new_state[0:2] - angle_array
-
-        tangent_pt_l = centre_l - self.radius * self.bound_l.norm_vec
-        tangent_pt_r = centre_r + self.radius * self.bound_r.norm_vec
 
         axis.cla()
         circle = plt.Circle(centre_l, self.radius, fill=False, linewidth=2)
@@ -143,46 +154,73 @@ class SafetyMask:
 
         axis.set_aspect('equal')
 
-        axis.plot([centre_l[0], tangent_pt_l[0]], [centre_l[1], tangent_pt_l[1]], 'g-')
-        axis.plot([centre_r[0], tangent_pt_r[0]], [centre_r[1], tangent_pt_r[1]], 'g-')
-        axis.plot(tangent_pt_l[0], tangent_pt_l[1], 'go')
-        axis.plot(tangent_pt_r[0], tangent_pt_r[1], 'go')
+        # check what poly I am in
+        pt = Point(state[0], state[1])
+        if not self.polys[self.bound_ind].contains(pt):
+            self.bound_ind += 1
+            if self.bound_ind == self.N: self.bound_ind = 0
 
-        #TODO: add condition to only check intersection if it is in front of the vehicle.
-        radial_line = [centre_l, tangent_pt_l]
-        if do_lines_intersect(self.bound_r.line, radial_line):
-            print("Unsafe - right")
-            axis.text(state[0]+2, 0, "Unsafe - right")
-            return self.max_steer
+            if self.polys[self.bound_ind].contains(pt):
+                print(f"{self.step_counter}: Switched to next poly: {self.bound_ind-1} --> {self.bound_ind}")
+            else:
+                print(f"{self.step_counter}: Left previous poly, but not in next..... issue")
 
-        radial_line = [centre_r, tangent_pt_r]
-        if do_lines_intersect(self.bound_l.line, radial_line):
-            print("Unsafe - left")
-            axis.text(state[0]+2, 0, "Unsafe - left")
-            return -self.max_steer
+        if self.bound_ind == self.N-1:
+            search_range = [self.bound_ind, 0]
+        else:
+            search_range = [self.bound_ind, self.bound_ind+1]
+        for i in search_range:
+            # i = self.bound_ind + z
+            tangent_pt_l = centre_l - self.radius * self.left_bounds[i].norm_vec
+            tangent_pt_r = centre_r + self.radius * self.right_bounds[i].norm_vec
 
-        axis.text(state[0]+2, 0, "Safe")
+
+            axis.plot([centre_l[0], tangent_pt_l[0]], [centre_l[1], tangent_pt_l[1]], 'g-')
+            axis.plot([centre_r[0], tangent_pt_r[0]], [centre_r[1], tangent_pt_r[1]], 'g-')
+            axis.plot(tangent_pt_l[0], tangent_pt_l[1], 'go')
+            axis.plot(tangent_pt_r[0], tangent_pt_r[1], 'go')
+
+            text_location = [2, 2]
+            #TODO: add condition to only check intersection if it is in front of the vehicle.
+            radial_line = [centre_l, tangent_pt_l]
+            if do_lines_intersect(self.right_bounds[i].line, radial_line):
+                print(f"{self.step_counter}: Unsafe - right")
+                axis.text(2, 2, "Unsafe - right")
+                return self.max_steer
+
+            radial_line = [centre_r, tangent_pt_r]
+            if do_lines_intersect(self.left_bounds[i].line, radial_line):
+                print(f"{self.step_counter}: Unsafe - left")
+                axis.text(2, 2, "Unsafe - left")
+                return -self.max_steer
+
+        axis.text(2, 2, "Safe")
         return action
 
 
 def run_test():
     max_steer = 0.4
-    line_r = [[1, 0], [1, 5]]
-    line_l = [[0, 0], [0, 5]]
-    # line_r = [[0, 0], [5, 0]]
-    # line_l = [[0, 1], [5, 1]]
-    # line_l = [[0, 1], [4, 5]]
-    # line_r = [[0, 0], [4, 4]]
-    sim = StraightAngleSim(line_l, line_r)
-    supervisor = SafetyMask(max_steer, state_update, line_l, line_r)
+
+    l1_l = [[1, 1], [4, 1]]
+    l2_l = [[4, 1], [4, 4]]
+    l3_l = [[4, 4], [1, 4]]
+    l4_l = [[1, 4], [1, 1]]
+    l1_r = [[0, 0], [5, 0]]
+    l2_r = [[5, 0], [5, 5]]
+    l3_r = [[5, 5], [0, 5]]
+    l4_r = [[0, 5], [0, 0]]
+    left_lines = [l1_l, l2_l, l3_l, l4_l]
+    right_lines = [l1_r, l2_r, l3_r, l4_r]
+
+    sim = BlockSim(left_lines, right_lines)
+    supervisor = SafetyMask(max_steer, state_update, left_lines, right_lines)
 
     actions = []
     safe_actions = []
     state = sim.reset()
     fig = plt.figure(figsize=(9, 9))
     a1 = plt.subplot(1, 1, 1)
-    # a3 = plt.subplot(2, 1, 2)
-    for i in range(80):
+    for i in range(350):
 
         action = np.random.uniform(-max_steer, max_steer)
         # action = max_steer 
@@ -194,17 +232,8 @@ def run_test():
 
         sim.render(a1)
 
-        # a3.cla()
-        # a3.plot(actions, 'b-')
-        # a3.plot(safe_actions, 'r-')
-        # a3.grid(True)
-        # a3.set_xlim(max(0, len(actions)-20), len(actions)+5)
-
         plt.tight_layout()
         plt.savefig(f"Data/History_{i}.svg")
-        # plt.pause(0.5)
-        # plt.pause(0.0001)
-        # plt.show()
 
 
 run_test()
